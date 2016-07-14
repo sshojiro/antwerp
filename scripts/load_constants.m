4504ea4411324c00be001a4c6247ce3c677c1df8
sshojiro <shojiro.shibayama@gmail.com>
2016-07-14 10:43:32 +0900

    [add] constant loader is added

---
+++ scripts/load_constants.m
@@ -0,0 +1,10 @@
+%% load_constants
+
+util = common.Utility;
+evaluator = common.Evaluation;
+
+env = common.loadConst('env');
+exp = common.loadConst('exp');
+litr_global = common.loadConst('litr');
+litr = litr_global.ja;
+data = common.loadConst('data');
